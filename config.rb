5637c6c908936c43611d5a6adec22ffb6453bdbd
Arunjit Singh <arunjit@me.com>
2013-03-17 19:36:13 +0530

    scss

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+#output_style = :compact
